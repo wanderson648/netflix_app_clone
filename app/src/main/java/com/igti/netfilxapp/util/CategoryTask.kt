bb02901b390d64194733fec101c46d5a826d267a
unknown <testpedroca@gmail.com>
2023-01-09 15:33:31 -0300

    Consumindo dados remotos: Leitura byte a byte (parte 2)

--- app/src/main/java/com/igti/netfilxapp/util/CategoryTask.kt
+++ app/src/main/java/com/igti/netfilxapp/util/CategoryTask.kt
@@ -1,7 +1,10 @@
 package com.igti.netfilxapp.util
 
 import android.util.Log
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.URL
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
@@ -25,16 +28,33 @@ class CategoryTask {
                 }
 
                 val stream = urlConnection.inputStream
-                val jsonAsString = stream.bufferedReader().use { it.readText() }
-                Log.i("Test", "execute: $jsonAsString")
+
+//                val jsonAsString = stream.bufferedReader().use { it.readText() }
+
+                val buffer = BufferedInputStream(stream)
+                val jsonAsString = toString(buffer)
+                Log.i("Test", jsonAsString)
                 
 
             } catch (e: IOException) {
                 Log.e("Test", e.message ?: "Erro desconhecido", e)
             }
 
-
         }
+    }
+
+    private fun toString(stream: InputStream): String {
+        val bytes = ByteArray(1024)
+        val baos = ByteArrayOutputStream()
+        var read: Int
+        while (true) {
+            read = stream.read(bytes)
+            if(read <= 0) {
+                break
+            }
+            baos.write(bytes, 0, read)
+        }
+        return String(baos.toByteArray())
     }
 
 }
